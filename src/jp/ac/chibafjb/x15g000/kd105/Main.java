6ef43fb55f0047f13af4999d5605aac817e02bed
unknown <x15g008@PC504N019.chiba-fjb.ac.jp>
2015-11-02 14:56:51 +0900

    105

---
+++ src/jp/ac/chibafjb/x15g000/kd105/Main.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g000.kd105;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+System.out.println("氏名: 伊藤　カイジ");
+System.out.println("職業: 無職");
+System.out.println("趣味:ギャンブル");
+	}
+
+}
