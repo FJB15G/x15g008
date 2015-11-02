9a7acd1d5be41b8905ba6a723e92e9e4070f1242
unknown <x15g008@PC504N019.chiba-fjb.ac.jp>
2015-11-02 15:22:46 +0900

    106

---
+++ src/jp/ac/chibafjb/x15g000/kd106/Main.java
@@ -0,0 +1,31 @@
+package jp.ac.chibafjb.x15g000.kd106;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+System.out.println("　　　　　　　　　　 　 　 -─‐- ､.　　 .. -─;-");
+System.out.println("　　　　　　　　　　-=ﾆ´￣　　 　 ヽ'´　∠´__");
+System.out.println("　　　　　　　　　,."+"´　　　　　　　　　　　　　 ｀＞");
+System.out.println("　　　　　　　　 ／　　　　　　　　　　　　　　 ￣｀ヽ.");
+System.out.println("　　　　　　　 /　　　　　　　　　　　　　　　　　 ヽ､ ヽ");
+System.out.println("　 　 　 　 　 i　　　　　　　　　　　 ∧　　　　 　 ＼ヽ!");
+System.out.println(".　　 　 　 　 |　　　　　　　　,　/| /:::::l　ﾄ､　　.　　ヽ");
+System.out.println("　　　　　　　|　　　　　　　 /|ｲ､l_;;:::::::|ﾉ_,.ゝ.　ﾄ､ ﾄ､l　　　ククク・・・・・・・・・・");
+System.out.println("　　　　　　　|　　　　　 ,ｲ/`''‐､_,ﾘ:::::::;ﾘ,. -ﾍﾄ | ヽ! `");
+System.out.println("　　　　　　　|　　, ‐､ r'＝=｡===;;;::::::(=｡=＝lヽ!　　　　　　いいのかよ・・・・！");
+System.out.println("　　　　　 　 | 　 { f､|.|::::`ｰ--‐'　　::::＼-‐' l");
+System.out.println("　　　　　　 ,'　　 ! ト||::::::u 　 u　 r __::::::＼'ｌ|　　　　　オレが行くとなったら・・・・・・");
+System.out.println(".　　　　　 /　　　`ｰ'ﾄ､::::. ｰ----------;ｦ│　　　　　　遊びじゃなくなる・・・・・・！");
+System.out.println("　　　 　 /　 ／|　　 | :＼:::.` ｰ-----‐'´ /　!");
+System.out.println("　　　,∠-‐/ 　 |.　　|　 :::＼:::.　ｰ--‐　 /!ヽ.ヽ　　　　張らせてもらうぜっ・・・・・・！");
+System.out.println("　 二 -‐''7　　　|　　|　 　 ::::＼::.　 　 , ' |　 ト､＼　　　　限界（2万ペリカ）を");
+System.out.println(".　　　　 /　　　　|.　 |　　　　 ::::`‐､／　ﾚ 　 ト､`''‐　　　　　　超えてっ・・・・・・！");
+System.out.println(".　　　　,'　　　 　 ﾄ.　|　　　　　　:::|　 ｲﾘ　　 :!　 `''‐");
+System.out.println("　　 　 i　　　　　 | ヽゝ　　　　　　|lW´'　　　 :l");
+System.out.println(". 　 　 |　　 ,. -‐''＾ヽ|`ヽ､..＿＿ノ|／⌒`‐､.　 l");
+System.out.println("　　　 レ''´　　　　　 |;';';';';';';';';';';';'|　　　　　`‐､!");
+System.out.println("　　 　 　 　 　 　 ○ |';';';';';';';';';';';'| ==");
+	}
+
+}
