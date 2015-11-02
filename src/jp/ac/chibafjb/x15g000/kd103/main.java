160966ccf81fc13517f9d616dd1802e2773e39a8
unknown <x15g008@PC504N019.chiba-fjb.ac.jp>
2015-11-02 14:49:06 +0900

    103

---
+++ src/jp/ac/chibafjb/x15g000/kd103/main.java
@@ -0,0 +1,10 @@
+package jp.ac.chibafjb.x15g000.kd103;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+System.out.println("1 船橋太郎 2 船橋太郎 3 船橋太郎 4 船橋太郎 5 船橋太郎 6 船橋 太郎 7 船橋太郎 8 船橋太郎 9 船橋太郎 10 船橋太郎");
+	}
+
+}
