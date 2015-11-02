e5713a0823db643ae602ff26dc8127c65d45c993
unknown <x15g008@PC504N019.chiba-fjb.ac.jp>
2015-11-02 14:53:23 +0900

    104

---
+++ src/jp/ac/chibafjb/x15g000/kd104/Main.java
@@ -0,0 +1,10 @@
+package jp.ac.chibafjb.x15g000.kd104;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+ System.out.println("あいうえお　あいうえお　あいうえお\nあいうえお　あいうえお　あいうえお\nあいうえお　あいうえお　あいうえお");
+	}
+
+}
