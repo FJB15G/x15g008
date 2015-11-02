87283b8b1c0267e429e0c14bd3e64735f704ea43
unknown <x15g008@PC504N019.chiba-fjb.ac.jp>
2015-11-02 14:26:37 +0900

    2015/11/2 初期ファイル１

---
+++ src/jp/ac/chibafjb/x15g000/kdx15g008/Main.java
@@ -0,0 +1,13 @@
+package jp.ac.chibafjb.x15g000.kdx15g008;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+ System.out.println("船橋情報ビジネス専門学校");
+ System.out.println("047-425-1051");
+ System.out.println("Hello☆こんにちは");
+
+	}
+
+}
