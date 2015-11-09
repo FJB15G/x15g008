4cf85372aec00b3500716e1e24e953825069f16f
unknown <x15g008@PC504N019.chiba-fjb.ac.jp>
2015-11-09 15:11:19 +0900

    133

---
+++ src/jp/ac/chibafjb/x15g008/kd131/main.java
@@ -0,0 +1,11 @@
+package jp.ac.chibafjb.x15g008.kd131;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+int x=10;
+System.out.println(x);
+	}
+
+}
---
+++ src/jp/ac/chibafjb/x15g008/kd133/main.java
@@ -0,0 +1,12 @@
+package jp.ac.chibafjb.x15g008.kd133;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+int x=5;
+int y=10;
+System.out.println("x+y="+(x+y));
+	}
+
+}
